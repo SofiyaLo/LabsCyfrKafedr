99e489b695174e191e09b116b45a9b48bc5bb680
SofiyaLo <sonyalopatina01@gmail.com>
2024-10-22 13:39:46 +0300

    Update task_1.1_lopatina.py
    Введена переменная отображающая пропущенный элемент

--- task_1.1_lopatina.py
+++ task_1.1_lopatina.py
@@ -1,5 +1,6 @@
 numbers = [2, -93, -2, 8, None, -44, -1, -85, -14, 90, -22, -90, -100, -8, 38, -92, -45, 67, 53, 25]
 
 # TODO заменить значение пропущенного элемента средним арифметическим
-numbers[4] = round((sum(numbers[:4]) + sum(numbers[5:])) / len(numbers) , 2)
+missing_num = 4
+numbers[missing_num] = round((sum(numbers[:missing_num]) + sum(numbers[missing_num + 1:])) / len(numbers) , 2)
 print("Измененный список:", numbers)
