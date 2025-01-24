d83b4c8ba4df8a5a1872f348dd5cf1db6f6ab1ad
SofiyaLo <sonyalopatina01@gmail.com>
2025-01-24 12:02:24 +0300

    Update OOP 2 class Book.py
    Исправлены названия id, теперь они id_

--- OOP 2 class Book.py
+++ OOP 2 class Book.py
@@ -14,8 +14,8 @@ BOOKS_DATABASE = [
 
 # TODO написать класс Book
 class Book:
-    def __init__(self, id: int, name: str, pages: int):
-        self.id = id
+    def __init__(self, id_: int, name: str, pages: int):
+        self.id_ = id_
         self.name = name
         self.pages = pages
 
@@ -23,12 +23,12 @@ class Book:
         return f'Книга "{self.name}"'
 
     def __repr__(self):
-        return f"Book(id_={self.id}, name='{self.name}', pages={self.pages})"
+        return f"Book(id_={self.id_}, name='{self.name}', pages={self.pages})"
 
 if __name__ == '__main__':
     # инициализируем список книг
     list_books = [
-        Book(id=book_dict["id"], name=book_dict["name"], pages=book_dict["pages"]) for book_dict in BOOKS_DATABASE
+        Book(id_=book_dict["id"], name=book_dict["name"], pages=book_dict["pages"]) for book_dict in BOOKS_DATABASE
     ]
     for book in list_books:
         print(book)  # проверяем метод __str__
